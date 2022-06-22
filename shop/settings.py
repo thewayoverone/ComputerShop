43f2a196229fc962c8567e15e7c419097380df6b
MaslovskyDen <maslovsky456@gmail.com>
2022-06-22 23:25:22 +0300

    add file

--- shop/settings.py
+++ shop/settings.py
@@ -69,7 +69,7 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'dbsqlite3',
     }
 }
 
@@ -96,7 +96,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 

2a3ded241e8d99cb991c2234c6f000c6bd18bc34
MaslovskyDen <maslovsky456@gmail.com>
2022-06-23 00:36:25 +0300

    add new application and session

--- shop/settings.py
+++ shop/settings.py
@@ -7,6 +7,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
+CART_SESSION_ID = 'cart'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -23,6 +25,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'myshop',
+    'cart',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -55,6 +58,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
